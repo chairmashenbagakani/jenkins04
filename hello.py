68a8e4aa49b8d2cb50435bb4062d49e2aff1c873
root <root@ip-172-31-9-114.us-east-2.compute.internal>
2025-11-04 13:36:23 +0000

    Rename hello.py.save to hello.py

---
+++ hello.py
@@ -0,0 +1,6 @@
+import datetime
+
+print("===== EC2 Output Test =====")
+print(f"Current time on EC2: {datetime.datetime.now()}")
+print("Hello! Jenkins fetched and ran this code.")
+print("============================")
